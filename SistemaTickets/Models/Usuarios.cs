781865739684480a8699a39cdc15305df6edeefb
Diego Hernandez <diego.hernandez4@catolica.edu.sv>
2025-05-22 22:23:09 -0600

    Cambiosm usuario

--- SistemaTickets/Models/Usuarios.cs
+++ SistemaTickets/Models/Usuarios.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SistemaTickets.Models
 {
@@ -6,11 +7,40 @@ namespace SistemaTickets.Models
     {
         [Key]
         public int UserId { get; set; }
-        public string Nombre { get; set; } 
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(100)]
         public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido")]
+        [StringLength(100)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "La contraseña debe tener al menos 8 caracteres, incluir una letra, un número y un carácter especial.")]
         public string Contrasena { get; set; }
+
+        [Required(ErrorMessage = "Seleccione un rol válido")]
         public int RolId { get; set; }
-        public Boolean TieneEmpresa { get; set; }
+
+        public bool TieneEmpresa { get; set; } = false;
+
+        // Campos para usuarios externos
+        [StringLength(200)]
+        public string NombreEmpresa { get; set; }
+
+        [StringLength(100)]
+        public string ContactoEmpresa { get; set; }
+
+        [Phone(ErrorMessage = "Ingrese un número de teléfono válido")]
+        [StringLength(20)]
+        public string TelefonoEmpresa { get; set; }
     }
 }
