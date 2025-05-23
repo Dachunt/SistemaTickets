3ed40ea4f9737f3e93ec39743e4d956847bb50f7
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-05-22 23:35:37 -0600

    Enviando Creacion de tickets

--- SistemaTickets/Controllers/TicketsController.cs
+++ SistemaTickets/Controllers/TicketsController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SistemaTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace SistemaTickets.Controllers
 {
@@ -43,26 +44,77 @@ namespace SistemaTickets.Controllers
         }
 
         // GET: Tickets/Create
+        //public IActionResult Create()
+        //{
+        //    ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "Nombre");
+        //    ViewData["UserId"] = new SelectList(_context.Usuarios, "UserId", "Nombre");
+        //    return View();
+        //}
+
+        // GET: Tickets/Create
         public IActionResult Create()
         {
+            ViewBag.Categorias = new SelectList(_context.Categorias.ToList(), "CategoriaId", "Nombre");
             return View();
         }
 
         // POST: Tickets/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TicketId,UserId,CategoriaId,NombreAplicacion,Descripcion,Prioridad,Estado,FechaCreacion")] Tickets tickets)
+        public async Task<IActionResult> Create(
+            [Bind("TicketId,UserId,CategoriaId,NombreAplicacion,Descripcion,Prioridad,Estado,FechaCreacion")] Tickets tickets,
+            IFormFile[] Archivos,
+            string NombreUsuario,
+            string TelefonoUsuario,
+            string EmailUsuario,
+            string Comentario)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(tickets);
                 await _context.SaveChangesAsync();
+
+                // Guardar archivos si existen
+                if (Archivos != null && Archivos.Length > 0)
+                {
+                    string rutaArchivos = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "archivos");
+                    if (!Directory.Exists(rutaArchivos))
+                        Directory.CreateDirectory(rutaArchivos);
+
+                    foreach (var archivo in Archivos)
+                    {
+                        if (archivo.Length > 0)
+                        {
+                            var nombreArchivo = Path.GetFileName(archivo.FileName);
+                            var rutaCompleta = Path.Combine(rutaArchivos, nombreArchivo);
+
+                            using (var stream = new FileStream(rutaCompleta, FileMode.Create))
+                            {
+                                await archivo.CopyToAsync(stream);
+                            }
+
+                            var archivoAdjunto = new ArchivosAdjuntos
+                            {
+                                TicketId = tickets.TicketId,
+                                NombreArchivo = nombreArchivo,
+                                RutaArchivo = "/archivos/" + nombreArchivo
+                            };
+
+                            _context.ArchivosAdjuntos.Add(archivoAdjunto);
+                        }
+                    }
+
+                    await _context.SaveChangesAsync();
+                }
+
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Categorias = new SelectList(_context.Categorias.ToList(), "CategoriaId", "Nombre", tickets.CategoriaId);
             return View(tickets);
         }
+
+
 
         // GET: Tickets/Edit/5
         public async Task<IActionResult> Edit(int? id)
